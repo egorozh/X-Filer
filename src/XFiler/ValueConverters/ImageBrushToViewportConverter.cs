1979e48627d56fb42a3cebaf4beb1784cb5f68b8
Egor Zheludkov <zheludkov.egor@mail.ru>
2021-10-01 22:58:03 +0300

    Fix background image brush

---
+++ src/XFiler/ValueConverters/ImageBrushToViewportConverter.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Media;
+
+namespace XFiler;
+
+internal sealed class ImageBrushToViewportConverter : BaseValueConverter
+{
+    public override object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (value is ImageSource imageSource)
+            return new Rect(new Point(0, 0), new Size(imageSource.Width, imageSource.Height));
+
+        return new Rect();
+    }
+}
