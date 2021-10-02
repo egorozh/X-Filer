8189e8d713078d758c8f67e6aa7d798b2532505c
Egor Zheludkov <zheludkov.egor@mail.ru>
2021-10-02 13:31:28 +0300

    fix datagrid unfocused bug

--- src/Plugins/XFiler.Base/Presenters/Grid/Controls/GridFilesPresenter.xaml.cs
+++ src/Plugins/XFiler.Base/Presenters/Grid/Controls/GridFilesPresenter.xaml.cs
@@ -5,5 +5,14 @@ public partial class GridFilesPresenter
     public GridFilesPresenter()
     {
         InitializeComponent();
+
+        this.Loaded += OnLoaded;
+    }
+
+    private void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        this.Loaded -= OnLoaded;
+
+        RectSelectDataGrid.Focus();
     }
 }
